f924e5cb2812ae7f074ae40bdd14f09562055eef
tbrunetti <tonya.brunetti@colorado.edu>
2020-10-28 20:13:21 -0600

    testing working with add step1

--- go_src/saige_testing_funcs.go
+++ go_src/saige_testing_funcs.go
@@ -12,6 +12,7 @@ import (
 	"io"
 	"math"
 	"runtime"
+	"time"
 )
 
 var wgChunk sync.WaitGroup
@@ -19,6 +20,7 @@ var chrLengths = make(map[string]int)
 var processQueue = make([]string, 0)
 var wgPipeline sync.WaitGroup
 var wgSmallChunk sync.WaitGroup
+var wgNull sync.WaitGroup
 
 func main() {
 	runtime.GOMAXPROCS(2)
@@ -32,6 +34,26 @@ func main() {
 	imputeDir := "/home/brunettt/Downloads/software/github_repos/CCPM_Biobank_GWAS_Pipeline/imputation_data"
 	bindPoint := "/home/brunettt/Downloads/software/github_repos/CCPM_Biobank_GWAS_Pipeline/"
 	container := "/home/brunettt/Downloads/software/github_repos/CCPM_Biobank_GWAS_Pipeline/SAIGE_v0.39_CCPM_biobank_singularity_recipe_file_10272020.simg"
+	/*
+	outDir := "/gpfs/scratch/brunettt/test_SAIGE/newSAIGE_test_07262020/test_new_pipeline/"
+	sparseGRM := "/gpfs/scratch/brunettt/test_SAIGE/newSAIGE_test_07262020/step0_GRM/Biobank.v1.3.eigenvectors.070620.reordered.LDpruned_relatednessCutoff_0.0625_103154_randomMarkersUsed.sparseGRM.mtx"
+	sampleIDFile := "/gpfs/scratch/brunettt/test_SAIGE/newSAIGE_test_07262020/step0_GRM/Biobank.v1.3.eigenvectors.070620.reordered.LDpruned_relatednessCutoff_0.0625_103154_randomMarkersUsed.sparseGRM.mtx.sampleIDs.txt"
+	phenoFile := "/gpfs/scratch/brunettt/test_SAIGE/newSAIGE_test_07262020/biobank_paper_pheWAS/pheWAS_CCPMbb_freeze_v1.3.txt" 
+	plink := "/gpfs/scratch/brunettt/test_SAIGE/newSAIGE_test_07262020/requiredData/LDprunedMEGA/Biobank.v1.3.eigenvectors.070620.reordered.LDpruned"
+	outPrefix := "GO_TEST_multiple_sclerosis_CCPMbb_freeze_v1.3"
+	plink := "/gpfs/scratch/brunettt/test_SAIGE/newSAIGE_test_07262020/requiredData/LDprunedMEGA/Biobank.v1.3.eigenvectors.070620.reordered.LDpruned"
+	trait := "binary"
+	pheno := "multiple_sclerosis"
+	invNorm := "FALSE"
+	covars := "PC1,PC2,PC3,PC4,PC5,SAIGE_GENDER,age"
+	sampleID := "FULL_BBID"
+	nThreads := "24"
+	sparseKin := "TRUE"
+	markers := "30"
+	outPrefix := "GO_TEST_multiple_sclerosis_CCPMbb_freeze_v1.3"
+	rel := "0.0625"
+	loco := "TRUE"
+	covTransform := "TRUE"*/
 	// end of variables
 
 
@@ -39,6 +61,9 @@ func main() {
  	start:= strings.TrimSpace(chroms[0])
  	end:= strings.TrimSpace(chroms[1])
 
+ 	/*wgNull.Add(1)
+	go nullModel(bindPoint,container,sparseGRM,sampleIDFile,phenoFile,plink,trait,pheno,invNorm,covars,sampleID,nThreads,sparseKin,markers,outPrefix,rel,loco,covTransform) 
+	*/
  	// chunk can run the same time as the null model; and the association analysis needs to wait for the null to finish and chunk to finish
     chunk(start,end,build,chromosomeLengthFile,imputeDir,imputeSuffix,bindPoint,container,chunkVariants)
     fmt.Printf("The final queue is:\n %v\n", processQueue)
@@ -126,7 +151,7 @@ func smallerChunk(chrom,build,imputeDir,imputeSuffix,bindPoint,container string,
 
 	// if chunk is larger than total variants in file, do not chunk and just add full imputation file to queue
 	if chunkVariants < varVal {
-		processQueue = append(processQueue, filepath.Join(imputeDir, chrom)+imputeSuffix)
+		processQueue = append(processQueue, chrom+imputeSuffix)
 		fmt.Printf("%v is queue and %d is variant value\n", processQueue, varVal)
 	}else {
 		for loopId := 1; loopId < maxLoops + 1; loopId++ {
@@ -140,7 +165,9 @@ func smallerChunk(chrom,build,imputeDir,imputeSuffix,bindPoint,container string,
 
 func processing (loopId,chunkVariants int, bindPoint,container,chrom,imputeDir,imputeSuffix string) {
 	defer wgSmallChunk.Done()
-	fmt.Printf("Processing %s, chunk %d\n", chrom, loopId)
+	t0 := time.Now()
+	formatted := fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d", t0.Year(), t0.Month(), t0.Day(),t0.Hour(), t0.Minute(), t0.Second())
+	fmt.Printf("[func(processing) %s] Processing %s, chunk %d\n", formatted, chrom, loopId)
 	
 	loopNum := strconv.Itoa(loopId)
 	upperVal := loopId*chunkVariants
@@ -177,10 +204,43 @@ func processing (loopId,chunkVariants int, bindPoint,container,chrom,imputeDir,i
 	}
 			
 	if varVal > 0 {
-		processQueue = append(processQueue, filepath.Join(imputeDir, chrom+"_")+loopNum+"_"+lowerValStr+"_"+upperValStr+"_"+imputeSuffix)
-		fmt.Printf("%s, chunk %s has successfully completed and has been added to the processing queue\n", chrom,loopNum)
+		processQueue = append(processQueue, chrom+"_"+loopNum+"_"+lowerValStr+"_"+upperValStr+"_"+imputeSuffix)
+		t1 := time.Now()
+		formatted := fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d", t1.Year(), t1.Month(), t1.Day(),t1.Hour(), t1.Minute(), t1.Second())
+		fmt.Printf("[func(processing) %s] %s, chunk %s has successfully completed and has been added to the processing queue. Time Elapsed: %.2f minutes\n", formatted,chrom,loopNum, time.Since(t0).Minutes())
 	}else{
-		fmt.Printf("%s is empty with value %d and will not be added to queue.\n", filepath.Join(imputeDir, chrom+"_")+loopNum+"_"+lowerValStr+"_"+upperValStr+"_"+imputeSuffix, varVal)
+		fmt.Printf("%s is empty with value %d and will not be added to queue.\n", chrom+"_"+loopNum+"_"+lowerValStr+"_"+upperValStr+"_"+imputeSuffix, varVal)
 	}
 }
 			
+
+/*func nullModel (bindPoint,container,sparseGRM,sampleIDFile,phenoFile,plink,trait,pheno,invNorm,covars,sampleID,nThreads,sparseKin,markers,outPrefix,rel,loco,covTransform string) {
+	defer wgNull.Done()
+
+	cmd := exec.Command("singularity", "run", "-B", bindPoint, container, "/usr/lib/R/bin/R/Rscript", "/opt/step1_fitNULLGLMM.R",
+		"--sparseGRMFile="+sparseGRM,
+		"--sparseGRMSampleIDFile="+sampleIDFile,
+		"--phenoFile="+phenoFile,
+		"--plinkFile="+plink,
+		"--traitType="+trait,
+		"--phenoCol="+pheno,
+		"--invNormalize="+invNorm,
+		"--covarColList="+covars,
+		"--sampleIDColinphenoFile="+sampleID,
+		"--nThreads="+nThreads,
+		"--IsSparseKin="+sparseKin,
+		"--numRandomMarkerforVarianceRatio="+markers,
+		"--skipModelFitting=False",
+		"--memoryChunk=5",
+		"--outputPrefix="+filepath.Join(outDir, outPrefix),
+		"--relatednessCutoff="+rel,
+		"--LOCO="+loco,
+		"--isCovariateTransform="+covTransform)
+
+	cmd.Run()
+	//err := cmd.Start()
+	//if err != nil {
+	//	fmt.Println("Oooops, error!\n %v", err)
+	//}
+	_ = cmd.Wait()
+}*/
