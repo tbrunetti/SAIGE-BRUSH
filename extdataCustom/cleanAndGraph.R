12cec8621b3910b9144420d5b3b8a242031aaf80
tbrunetti <tonya.brunetti@colorado.edu>
2020-10-29 22:43:09 -0600

    speed optimized and output optimized

--- extdataCustom/cleanAndGraph.R
+++ extdataCustom/cleanAndGraph.R
@@ -12,17 +12,31 @@
 #write_tsv(infoFile, "allAutosomes.rs70.info.SAIGE.txt", append = FALSE, col_names = TRUE)
 
 
-cleanAndGraph <- function(assocFile, infoFile, dataOutputPrefix, macFilter, mafFilter, traitType){
+cleanAndGraph <- function(assocFile, infoFile, dataOutputPrefix, pheno, covars, macFilter, mafFilter, traitType, nThreads){
   library(qqman)
   library(data.table)
   library(readr)
   library(plyr)
+  library(png)
+  library(gridExtra)
+  library(ggplot2)
+  library(grid)
+  library(ggplotify)
   
+  assocFile="~/SAIGE/extdataCustom/ADMIRE_AD_CCPMbb_freeze_v1.3_chrm21_associationAnalysis_results.txt"
+  infoFile="~/allAutosomes.rs70.info.SAIGE.txt"
+  dataOutputPrefix="test_new_graph"
+  macFilter="10"
+  mafFilter="0.05"
+  traitType="binary"
+  nThreads=3
+  covars="PC1,PC2,PC3,PC4,PC5,SAIGE_GENDER,age"
+  pheno="multiple_sclerosis"
   # read in concatenated SAIGE association results across all 
-  autosomalAssocResults <- fread(assocFile, header=T)
+  autosomalAssocResults <- fread(assocFile, header=T, showProgress = T, nThread = nThreads)
   
   # merge info fields
-  infoFields <- fread(infoFile , header = T)
+  infoFields <- fread(infoFile , header = T, showProgress = T, nThread = nThreads)
   
   # perform a left only merge on info fields as long as all 4 columns match and only use the first match
   autosomalAssocResults <- join(x=autosomalAssocResults, y=infoFields, match = "first", type="left", by=c("CHR", "POS", "Allele1", "Allele2"))
@@ -87,17 +101,20 @@ cleanAndGraph <- function(assocFile, infoFile, dataOutputPrefix, macFilter, mafF
                                         "controlHomMinor", "controlHet", "negLog10pvalue","R2", "ER2", "GENOTYPE_STATUS")
     
     # write a copy of unfilterd table
-    write_tsv(autosomalAssocResults, paste(dataOutputPrefix, "GWASresults_allSNPs_noFiltering.txt", sep="_"), append=FALSE, col_names=TRUE)
+    fwrite(autosomalAssocResults, file = paste(dataOutputPrefix, "GWASresults_allSNPs_noFiltering.txt.gz", sep="_"), append=FALSE, col.names=TRUE, showProgress = T, nThread = nThreads, compress = "gzip", sep = "\t")
+    #write_tsv(autosomalAssocResults, paste(dataOutputPrefix, "GWASresults_allSNPs_noFiltering.txt", sep="_"), append=FALSE, col_names=TRUE)
     
     # filter by MAC and MAF based on user input to generate filtered common variants file (>macFilter, >mafFilter)
     commonClean <- autosomalAssocResults[which(as.numeric(autosomalAssocResults$MAC) > macFilter & 
                                                  as.numeric(autosomalAssocResults$MAF) > mafFilter),]
-    write_tsv(commonClean, paste(dataOutputPrefix, "GWASresults_commonSNPs_cleaned.txt", sep="_"), append=FALSE, col_names=TRUE)
+    fwrite(commonClean, file = paste(dataOutputPrefix, "GWASresults_commonSNPs_cleaned.txt.gz", sep="_"), append=FALSE, col.names = T, showProgress = T, nThread = nThreads, compress = "gzip", sep = "\t")
+    #write_tsv(commonClean, paste(dataOutputPrefix, "GWASresults_commonSNPs_cleaned.txt", sep="_"), append=FALSE, col_names=TRUE)
 
     # filter by MAC and MAF based on user input to generate filtered rare variants file (>macFilter, <=mafFilter)
     rareClean <- autosomalAssocResults[which(as.numeric(autosomalAssocResults$MAC) > macFilter & 
                                                as.numeric(autosomalAssocResults$MAF) <= mafFilter),]
-    write_tsv(rareClean, paste(dataOutputPrefix, "GWASresults_rareSNPs_cleaned.txt", sep="_"), append=FALSE, col_names=TRUE)
+    fwrite(rareClean, file = paste(dataOutputPrefix, "GWASresults_rareSNPs_cleaned.txt.gz", sep="_"), append=FALSE, col.names=TRUE, showProgress = T, nThread = nThreads, compress = "gzip", sep = "\t")
+    #write_tsv(rareClean, paste(dataOutputPrefix, "GWASresults_rareSNPs_cleaned.txt", sep="_"), append=FALSE, col_names=TRUE)
   }
   
   
@@ -111,18 +128,22 @@ cleanAndGraph <- function(assocFile, infoFile, dataOutputPrefix, macFilter, mafF
       names(autosomalAssocResults) <- c("CHR", "POS", "majorAllele", "minorAllele", "SNPID", "BETA", "SE", "OR", "LogOR", "Lower95OR", "Upper95OR", 
                                         "MAF", "MAC", "p.value", "N", "negLog10pvalue",  "R2", "ER2", "GENOTYPE_STATUS")
     # write a copy of unfilterd table
-    write_tsv(autosomalAssocResults, paste(dataOutputPrefix, "GWASresults_allSNPs_noFiltering.txt", sep="_"), append=FALSE, col_names=TRUE)
+      fwrite(autosomalAssocResults, file = paste(dataOutputPrefix, "GWASresults_allSNPs_noFiltering.txt", sep="_"), append=FALSE, col.names=TRUE, showProgress = T, nThread = nThreads, compress = "gzip", sep = "\t")
+      #write_tsv(autosomalAssocResults, paste(dataOutputPrefix, "GWASresults_allSNPs_noFiltering.txt", sep="_"), append=FALSE, col_names=TRUE)
     
     # filter by MAC and MAF based on user input to generate filtered common variants file (>macFilter, >mafFilter)
     commonClean <- autosomalAssocResults[which(as.numeric(autosomalAssocResults$MAC) > macFilter & 
                                                  as.numeric(autosomalAssocResults$MAF) > mafFilter),]
-    write_tsv(commonClean, paste(dataOutputPrefix, "GWASresults_commonSNPs_cleaned.txt", sep="_"), append=FALSE, col_names=TRUE)
+    fwrite(commonClean, file = paste(dataOutputPrefix, "GWASresults_commonSNPs_cleaned.txt", sep="_"), append=FALSE, col.names=TRUE, showProgress = T, nThread = nThreads, compress = "gzip", sep = "\t")
+    #write_tsv(commonClean, paste(dataOutputPrefix, "GWASresults_commonSNPs_cleaned.txt", sep="_"), append=FALSE, col_names=TRUE)
 
     
     # filter by MAC and MAF based on user input to generate filtered rare variants file (>macFilter, <=mafFilter)
     rareClean <- autosomalAssocResults[which(as.numeric(autosomalAssocResults$MAC) > macFilter & 
                                                as.numeric(autosomalAssocResults$MAF) <= mafFilter),]
-    write_tsv(rareClean, paste(dataOutputPrefix, "GWASresults_rareSNPs_cleaned.txt", sep="_"),append=FALSE, col_names=TRUE)
+    
+    fwrite(rareClean, file = paste(dataOutputPrefix, "GWASresults_rareSNPs_cleaned.txt", sep="_"), append=FALSE, col.names=TRUE, showProgress = T, nThread = nThreads, compress = "gzip", sep = "\t")
+    #write_tsv(rareClean, paste(dataOutputPrefix, "GWASresults_rareSNPs_cleaned.txt", sep="_"),append=FALSE, col_names=TRUE)
 
   }
  
@@ -132,14 +153,14 @@ cleanAndGraph <- function(assocFile, infoFile, dataOutputPrefix, macFilter, mafF
   lobs <- -(log10(observed))
   expected <- c(1:length(observed)) 
   lexp <- -(log10(expected / (length(expected)+1)))
-  bmp(filename=paste(dataOutputPrefix, "qqplot_commonSNPs_cleaned.bmp", sep="_"), width=800, height=800, bg="white", type="cairo")
+  png(filename=paste(dataOutputPrefix, "qqplot_commonSNPs_cleaned.png", sep="_"), width=800, height=800, bg="white", type="cairo")
   par(mar=c(5,6,4,1) + .1)
   plot(c(0,10), c(0,10), col="red", lwd=3, type="l", xlab="Expected (-logP)", ylab="Observed (-logP)", xlim=c(0,10), ylim=c(0,10), las=1, xaxs="i", yaxs="i", bty="l", cex.axis=2.0, cex.lab=2.0)
   points(lexp, lobs, pch=23, cex=.4, bg="black") 
   #inflation factor lambda
   chisq2 <- qchisq(1- commonClean$p.value,1,lower.tail = T)
   lambda <- median(chisq2,na.rm=T)/qchisq(0.5,1)#lambda1
-  mtext(bquote(lambda == .(lambda)), side=3, cex=2.0)
+  mtext(bquote(paste("QQ Plot for SNPs MAC >", .(macFilter), " and MAF > ", .(mafFilter), ":  ", lambda == .(lambda))), side=3, cex=2.0)
   dev.off()
   
   
@@ -148,19 +169,19 @@ cleanAndGraph <- function(assocFile, infoFile, dataOutputPrefix, macFilter, mafF
   lobs <- -(log10(observed))
   expected <- c(1:length(observed)) 
   lexp <- -(log10(expected / (length(expected)+1)))
-  bmp(filename=paste(dataOutputPrefix, "qqplot_rareSNPs_cleaned.bmp", sep="_"), width=800, height=800, bg="white", type="cairo")
+  png(filename=paste(dataOutputPrefix, "qqplot_rareSNPs_cleaned.png", sep="_"), width=800, height=800, bg="white", type="cairo")
   par(mar=c(5,6,4,1) + .1)
   plot(c(0,10), c(0,10), col="red", lwd=3, type="l", xlab="Expected (-logP)", ylab="Observed (-logP)", xlim=c(0,10), ylim=c(0,10), las=1, xaxs="i", yaxs="i", bty="l", cex.axis=2.0, cex.lab=2.0)
   points(lexp, lobs, pch=23, cex=.4, bg="black") 
   #inflation factor lambda
   chisq2 <- qchisq(1- rareClean$p.value,1,lower.tail = T)
   lambda <- median(chisq2,na.rm=T)/qchisq(0.5,1)#lambda1
-  mtext(bquote(lambda == .(lambda)), side=3, cex=2.0)
+  mtext(bquote(paste("QQ Plot for SNPs MAC >", .(macFilter), " and MAF <= ", .(mafFilter), ":  ", lambda == .(lambda))), side=3, cex=2.0)
   dev.off()
   
   #common and clean Manhattan plot
   upperYlim <- max(c(15, max(commonClean$negLog10pvalue)))
-  bmp(filename=paste(dataOutputPrefix, "manhattan_commonSNPs_cleaned.bmp", sep="_"), width=800, height=600, bg="white", type="cairo")
+  png(filename=paste(dataOutputPrefix, "manhattan_commonSNPs_cleaned.png", sep="_"), width=800, height=600, bg="white", type="cairo")
   par(font.axis = 2)
   substringPath <- strsplit(dataOutputPrefix, split = "/")
   prefix <- substringPath[[1]][length(substringPath[[1]])]
@@ -174,16 +195,18 @@ cleanAndGraph <- function(assocFile, infoFile, dataOutputPrefix, macFilter, mafF
             suggestiveline = -log10(1e-05), 
             genomewideline = -log10(5e-08),
             ylim=c(0,upperYlim),
-            cex.lab=1,
+            cex.lab=1.5,
+            cex.main=2.0,
             ylab="-log10(pvalue)",
             xlab="",
-            main=paste(prefix, ": MAC>", macFilter, ", MAF>", mafFilter, sep=" ")
+            annotateTop = T,
+            main=paste(prefix, ": MAC >", macFilter, ", MAF>", mafFilter, sep=" ")
   )
   dev.off()
   
   # rare and clean manhattan plot
   upperYlim <- max(c(15, max(rareClean$negLog10pvalue)))
-  bmp(filename=paste(dataOutputPrefix, "manhattan_rareSNPs_cleaned.bmp", sep="_"), width=800, height=600, bg="white", type="cairo")
+  png(filename=paste(dataOutputPrefix, "manhattan_rareSNPs_cleaned.png", sep="_"), width=800, height=600, bg="white", type="cairo", )
   par(font.axis = 2)
   substringPath <- strsplit(dataOutputPrefix, split = "/")
   prefix <- substringPath[[1]][length(substringPath[[1]])]
@@ -197,10 +220,26 @@ cleanAndGraph <- function(assocFile, infoFile, dataOutputPrefix, macFilter, mafF
             suggestiveline = -log10(1e-05), 
             genomewideline = -log10(5e-08),
             ylim=c(0,upperYlim),
-            cex.lab=1,
+            cex.lab=1.5,
+            cex.main=2.0,
             ylab="-log10(pvalue)",
             xlab="",
-            main=paste(prefix, ": MAC>", macFilter, ", MAF<=", mafFilter, sep=" ")
+            annotateTop=T,
+            main=paste(prefix, ": MAC >", macFilter, ", MAF <=", mafFilter, sep=" ")
   )
   dev.off()
+  
+  images <- lapply(list(paste(dataOutputPrefix, "qqplot_commonSNPs_cleaned.png", sep="_"),
+                        paste(dataOutputPrefix, "qqplot_rareSNPs_cleaned.png", sep="_"),
+                        paste(dataOutputPrefix, "manhattan_commonSNPs_cleaned.png", sep="_"),
+                        paste(dataOutputPrefix, "manhattan_rareSNPs_cleaned.png", sep="_")),
+                   png::readPNG)
+  
+  imageGrid <- lapply(images, grid::rasterGrob)
+
+  tmp<-do.call(gridExtra::grid.arrange, c(imageGrid, top=paste(pheno, "~", stringr::str_replace_all(covars, ",", " + "), "\n cases: ", as.character(autosomalAssocResults$N.Cases[1]), 
+                                                               "  controls:", as.character(autosomalAssocResults$N.Controls[1]), sep = " ")))
+  
+  
+  ggsave(file=paste(dataOutputPrefix, "_finalGWASresults.pdf", sep = ""), tmp, width = 10, height = 10, units = "in")
 }
