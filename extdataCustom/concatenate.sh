cef202a285947d6848bbcd37834aa7a04a4f11c6
tbrunetti <tonya.brunetti@colorado.edu>
2020-11-01 02:15:22 -0700

    annotate conctenate and sort plus remove dups

--- extdataCustom/concatenate.sh
+++ extdataCustom/concatenate.sh
@@ -6,16 +6,20 @@ for assocs in ${1}*_SNPassociationAnalysis.txt
 do
 	if [ $firstEncounter -eq 0 ]
 	then
-		head -n 1 $assocs > ${1}"_allChromosomeResultsMerged.txt"
-		tail -n+2 $assocs >> ${1}"_allChromosomeResultsMerged.txt"
+		head -n 1 $assocs > ${1}"_header_allChromosomeResultsMerged.txt"
+		tail -n+2 $assocs >> ${1}"_tmp_allChromosomeResultsMerged.txt"
 		echo "$assocs finished merging. Exit status is $?."
 		wait
 		((firstEncounter++))
 	fi
-	tail -n+2 $assocs >> ${1}"_allChromosomeResultsMerged.txt"
-	echo "$assocs finished merging. Exit status is $?."
+	tail -n+2 $assocs >> ${1}"_tmp_allChromosomeResultsMerged.txt"
+	sort ${1}"_tmp_allChromosomeResultsMerged.txt" | uniq | > ${1}"_tmp_allChromosomeResultsMerged_sorted.txt"
+	cat ${1}"_header_allChromosomeResultsMerged.txt" ${1}"_tmp_allChromosomeResultsMerged_sorted.txt" > ${1}"_allChromosomeResultsMerged.txt"
+	rm ${1}"_tmp_allChromosomeResultsMerged.txt"
+	rm ${1}"_header_allChromosomeResultsMerged.txt"
+	echo "[func(main) -- Concatentate] $assocs finished merging. Exit status is $?."
 	wait
 done
 
-echo "All files have been merged into single file. Exit status is $?."
+echo "[func(main) -- Concatenate] All files have been merged into single file. Exit status is $?."
 
